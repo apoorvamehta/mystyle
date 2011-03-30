f77cdd557ba1ae0625a973efe913a7da4505a4a1
apoorvamehta <apoora.mehta@gmail.com>
2011-03-30 16:34:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystyle::Application.config.secret_token = 'b7ec9c31ea13680c6a803aff68af3051811b25415a6034b184c99db71b0467ff1344bde835c04abca09ec68331fb3ff6fa5f7a79acf797ca869d142a06530f49'
